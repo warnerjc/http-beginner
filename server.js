eb89baf48274580da8be12c957d65e57e5a860cb
Jason Warner <jcwarner@gmail.com>
2018-04-21 10:54:31 -0500

    first simple localhost http server

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require("http");
+
+const goodPort = 7000;
+const badPort = 7500;
+
+function goodRequest(request, response) {
+    response.end("You are such an amazing person! Welcome!");
+}
+
+function badRequest(request, response) {
+    response.end("Uggh - why did we even let you in here!");
+}
+
+let goodServer = http.createServer(goodRequest);
+let badServer = http.createServer(badRequest);
+
+goodServer.listen(goodPort, function () {
+    console.log("Server listening on: http://localhost:" + goodPort);
+});
+
+badServer.listen(badPort, function() {
+    console.log("Server listening on: http://localhost:" + badPort);
+});
